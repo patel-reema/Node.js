6aa109782e9e00c87efab0809856a077efa4c761
yashvivadodariya <yashvivadodariya72@gmail.com>
2026-02-13 12:19:47 +0530

    pr

--- Admin-panel/controller/admin.controller.js
+++ Admin-panel/controller/admin.controller.js
@@ -1,8 +1,10 @@
 const Admin = require('../model/admin.model');
+const path = require('path');
+const fs = require('fs');
 
 exports.addAdminPage = async (req, res) => {
     try {
-        return res.render('admin/addAdmin');
+        return res.render('admin/addAdmin', { admin: null });
     } catch (error) {
         console.log(error);
         return res.redirect('/');
@@ -32,4 +34,65 @@ exports.viewAllAdmin = async (req, res) => {
         console.log(error);
         return res.redirect('/');
     }
+}
+
+exports.deleteAdmin = async (req, res) => {
+    let id = req.params.id;
+    let admin = await Admin.findById(id);
+
+    if (!admin) {
+        console.log("Admin not found..");
+        return res.redirect('/');
+    }
+
+    if (admin.profileimg != "") {
+        let imgpath = path.join(__dirname, "..", admin.profileimg);
+        try {
+            fs.unlinkSync(imgpath);
+        } catch (error) {
+            console.log('Something is missing');
+        }
+    }
+
+    await Admin.findByIdAndDelete(id);
+    return res.redirect('/admin/view-admin');
+}
+
+exports.editAdmin = async (req, res) => {
+    let admin = await Admin.findById(req.params.id);
+    if (!admin) {
+        console.log("Admin not found...");
+        return res.redirect('/');
+    }
+    return res.render('admin/addAdmin', { admin });
+}
+
+exports.updateAdmin = async (req, res) => {
+    let admin = await Admin.findById(req.params.id);
+
+    if (!admin) {
+        console.log('Admin is not found...')
+        return res.redirect('/');
+    }
+
+    let imgpath;
+
+    if (req.file) {
+        if (admin.profileimg != "") {
+            imgpath = path.join(__dirname, '..', admin.profileimg);
+            try {
+                fs.unlinkSync(imgpath);
+            } catch {
+                console.log('file missing');
+            }
+        }
+
+        imgpath = `/uploads/${req.file.filename}`;
+    }
+    else {
+        imgpath = admin.profileimg;
+    }
+
+    admin = await Admin.findByIdAndUpdate(admin._id, { ...req.body, profileimg: imgpath }, { new: true });
+    return res.redirect('/');
 }
--- Admin-panel/routes/admin.routes.js
+++ Admin-panel/routes/admin.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { addAdminPage, addAdmin, viewAllAdmin } = require('../controller/admin.controller');
+const { addAdminPage, addAdmin, viewAllAdmin, deleteAdmin, updateAdmin, editAdmin } = require('../controller/admin.controller');
 const uploadImg = require('../middleware/uploadImg');
 const routes = express.Router();
 
@@ -7,5 +7,8 @@ const routes = express.Router();
 routes.get('/add-admin', addAdminPage);
 routes.post('/add-admin', uploadImg.single('profileImg'), addAdmin);
 routes.get('/view-admin', viewAllAdmin);
+routes.get('/edit-admin', editAdmin);
+routes.get('/update-admin', updateAdmin);
+routes.get('/delete-admin/:id', deleteAdmin)
 
 module.exports = routes;
